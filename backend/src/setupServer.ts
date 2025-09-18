68c7ea29012b3b8f1018c0cc4d01bfd6220d05d8
nikoicardo <nikoicardo@gmail.com>
2025-09-18 11:40:58 -0700

    Create Application class in app.ts

--- backend/src/setupServer.ts
+++ backend/src/setupServer.ts
@@ -15,6 +15,8 @@ import HTTP_STATUS from "http-status-codes";
 import compression from "compression";
 import "express-async-errors";
 
+const SERVER_PORT = 5000;
+
 export class Server {
   private app: Application;
   constructor(app: Application) {
@@ -60,9 +62,20 @@ export class Server {
 
   private globalErrorHandler(app: Application): void {}
 
-  private startServer(app: Application): void {}
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      this.startHttpServer(httpServer);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   private createSocketIO(httpServer: http.Server): void {}
 
-  private startHttpServer(httpServer: http.Server): void {}
+  private startHttpServer(httpServer: http.Server): void {
+    httpServer.listen(SERVER_PORT, () => {
+      console.log(`Server running on port: ${SERVER_PORT}`);
+    });
+  }
 }
