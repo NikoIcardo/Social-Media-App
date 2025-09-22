89f59879731f1081862adfe2353172a2282cc659
nikoicardo <nikoicardo@gmail.com>
2025-09-22 09:51:37 -0700

    add cloudinary, cloudinary api config, and a cloudinary uploads function

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import bunyan from "bunyan";
+import cloudinary from "cloudinary";
 
 dotenv.config({});
 
@@ -12,6 +13,9 @@ class Config {
   public CLIENT_URL: string | undefined;
   public SERVER_PORT: string | undefined;
   public REDIS_HOST: string | undefined;
+  public CLOUD_NAME: string | undefined;
+  public CLOUD_API_KEY: string | undefined;
+  public CLOUD_API_SECRET: string | undefined;
 
   private readonly DEFAULT_DATABASE_URL =
     "mongodb://localhost:27017/social-media-app-backend";
@@ -25,6 +29,9 @@ class Config {
     this.CLIENT_URL = process.env.DATABASE_URL || "";
     this.SERVER_PORT = process.env.SERVER_PORT || "";
     this.REDIS_HOST = process.env.REDIS_HOST || "";
+    this.CLOUD_NAME = process.env.CLOUD_NAME || "";
+    this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || "";
+    this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || "";
   }
 
   public createLogger = (name: string): bunyan => {
@@ -38,6 +45,14 @@ class Config {
       }
     });
   };
+
+  public cloudinaryConfig(): void {
+    cloudinary.v2.config({
+      cloud_name: this.CLOUD_NAME,
+      api_key: this.CLOUD_API_KEY,
+      api_secret: this.CLOUD_API_SECRET,
+    });
+  }
 }
 
 export const config: Config = new Config();
